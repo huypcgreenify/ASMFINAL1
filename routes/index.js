1a053594daf5ff27ca07465bb58f2850f22ae5b2
huypro123 <62203494+huypro123@users.noreply.github.com>
2021-04-18 19:17:31 +0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,144 @@
+var express = require('express');
+var router = express.Router();
+
+var dbConnect = 'mongodb+srv://admin:admin@cluster0.4zl2i.mongodb.net/tinder';
+// getting-started.js
+const mongoose = require('mongoose');
+mongoose.connect(dbConnect, {useNewUrlParser: true, useUnifiedTopology: true});
+
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'assets/img/portfolio/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + Math.round(Math.random() * 1E9) + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1]);
+    },
+})
+
+var upload = multer({
+    dest: 'assets/img/portfolio/'
+    , storage: storage
+})
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+    console.log('connected')
+});
+
+var user = new mongoose.Schema({
+    avatar: String,
+    username: String,
+    password: String,
+    name: String,
+    address: String,
+    number_phone: String,
+})
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+router.get('/dn', function (req, res, next) {
+    res.render('dn', {title: 'Express'});
+});
+router.get('/dk', function (req, res, next) {
+    res.render('dk', {title: 'Express'});
+});
+
+router.get('/qlu', upload.single('avatar'), function (req, res, next) {
+
+    var userConnect = db.model('users', user);
+
+    userConnect.find({}, function (error, users) {
+        var type = 'home';
+        try {
+            type = req.query.type;
+        } catch (e) {
+        }
+        if (error) {
+            res.render('qlu', {title: 'Express : ' + error});
+            return
+        }
+        if (type == 'json') {
+            res.send(users)
+        } else {
+            res.render('qlu', {title: 'Quản lý User', users: users});
+        }
+    });
+
+});
+
+//Thêm
+router.post('/insertUser', upload.single('avatar'), function (req, res) {
+    var userConnect = db.model('users', user);
+    var run = userConnect({
+        avatar: req.file.filename,
+        username: req.body.username,
+        password: req.body.password,
+        name: req.body.name,
+        address: req.body.address,
+        number_phone: req.body.number_phone,
+
+    }).save(function (error) {
+        if (error) {
+            res.render('index', {title: 'Express : Error!!!!'});
+        } else {
+            // res.render('index', {title: 'Express : Success!!!!'});
+            res.redirect('qlu');
+            // res.send(req.file.filename)
+
+        }
+    })
+})
+//Sửa
+router.get('/edit/:id', function (req, res) {
+    var userConnect = db.model('users', user);
+    userConnect.findOneAndUpdate({_id: req.params.id}, res.body, {new: true}, (error, docs) => {
+        if (error) {
+            res.render('qlu', {title: 'Express : Error!!!!'});
+        } else {
+            res.render('edit', {userConnect: docs});
+
+        }
+    })
+})
+
+router.post('/edit/:id', upload.single('avatar'), function (req, res) {
+    var userConnect = db.model('users', user);
+    userConnect.findByIdAndUpdate({_id: req.params.id},
+        {
+            avatar: req.file.filename,
+            username: req.body.username,
+            password: req.body.password,
+            name: req.body.name,
+            address: req.body.address,
+            number_phone: req.body.number_phone
+        }).update(function (error) {
+        if (error) {
+            console.log("Sửa thất bại")
+        } else {
+            console.log("Sửa thành công")
+            // res.render('edit', {userConnect: docs});
+            res.redirect('/qlu')
+        }
+    })
+})
+//Xóa
+router.post('/:id', function (req, res) {
+    var userConnect = db.model('users', user);
+    var id = req.params.id;
+    userConnect.remove({_id: id}, function (error) {
+        if (error) {
+            res.render('index', {title: 'Express : Error!!!!'});
+        } else {
+            console.log("Xóa thành công")
+            res.redirect('qlu')
+
+        }
+    })
+
+})
+
+module.exports = router;
